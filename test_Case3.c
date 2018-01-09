ac5c97e734fb983667991458c4a56f1c436bd880
Tarun Karthik <tarunkarthik999@gmail.com>
2018-01-10 03:18:20 +0530

    Add files via upload

---
+++ test_Case3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#define X 10
+
+// first program ever!!!!
+
+void main()
+{
+	printf("\nHello World");
+	do
+	{
+		/* do something random
+			i said do something random
+				nevermind ...	*/
+	}while(1);
+}
