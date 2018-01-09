4b82c93b8ef055bf7ea558a2895be889da0e90f1
Akshay Honrao <eakshay97honrao@gmail.com>
2018-01-09 22:36:04 +0530

    Add test file

---
+++ Test.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+void func(){
+	//Single line comment
+}
+
+int main(){	
+	int +p, a;
+	char c;
+	float b;
+	
+	/*
+	Here is the 
+	multiline comment	
+	*/
+
+	func();     //Function	
+	
+	scanf("%d",&a );
+	scanf("%f",&b );
+	
+	if(a == 0){
+		printf("a is zero\n" );
+	}
+	else
+	{
+		printf("a is non zero\n");
+	}
+	
+	while(a){
+		
+		while(b){
+			
+			printf("Inner while loop\n");
+			
+			b=b-1.0;
+		}
+		
+		printf("Outer while loop\n");
+		a/=2;
+		
+	}
+	
+	return 0;
+}
+}
